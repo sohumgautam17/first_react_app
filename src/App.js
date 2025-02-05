b6260de671a731af71e902abdc408971aef51be2
Sohum Gautam <sohumgautam@mac.mynetworksettings.com>
2025-02-04 22:06:58 -0500

    create movie app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-// import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App...</h1>
-    </div>
-  );
+import  './App.css';
+import SearchIcon from './search.svg';
+
+
+// api key: 74abaa4b
+
+const API_URL = 'http://www.omdbapi.com/?apikey=74abaa4b';
+
+const App = () => {
+
+    const fetchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        fetchMovies('batman')
+    }, [])
+
+    return (
+        <div className='App'>
+            <h1>MovieLand</h1>
+        </div>
+    );
 }
 
 export default App;
